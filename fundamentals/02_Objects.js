7558af910160a4ba589580b6afdb7526ddd5533c
Shubhangi Bawanthade <82374149+shubhangi-bawanthade@users.noreply.github.com>
2023-12-12 08:05:07 +0000

    add objects

---
+++ fundamentals/02_Objects.js
@@ -0,0 +1,26 @@
+// let user={
+//     address:{
+//         street:"ram nagar"
+//     }
+// }
+// console.log(user.address.street)
+
+// console.log(user && user.address && user.address.street);
+
+// console.log(user ?.address ?.street);
+
+// key="firstName"
+
+// let user={
+//     firstName:"neha"
+// }
+// console.log(user?.[key]); 
+// console.log(user?.firstName);
+
+let user={
+    lastname:"kumar"
+}
+
+user?.name="john";
+
+console.log(user.name);
