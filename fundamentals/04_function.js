e1ec6578627eab2e75ef4a59ffae7fdc7a174055
Shubhangi Bawanthade <82374149+shubhangi-bawanthade@users.noreply.github.com>
2023-12-20 08:20:19 +0000

    add functions

---
+++ fundamentals/04_function.js
@@ -0,0 +1,56 @@
+
+
+
+// var c=300;
+
+// function loginUser(username="sham")
+// {
+//     console.log(`${username} is current user` );
+//     c=50
+// }
+
+// loginUser()
+// console.log(c);
+
+// function chai()
+// {
+//     console.log(this);
+// }
+
+// chai()
+
+// const chai1 = () =>{
+//     let name="hello"
+//this  {}
+//     console.log(this.name); 
+// }
+
+// chai1()
+
+// const add = (num1,num2) =>
+// {
+//     console.log(num1+num2);
+// }
+
+// add(2,3)
+
+// const add= (num1,num2) => (num1 +num2)
+
+// console.log(add(2,4));
+
+//Immediatly Invoked Function Expression(IIFE)
+
+(function fun1()
+ {
+    //  named IIFE
+     console.log("welcome");
+ })();
+
+(()=>{
+    console.log("welcome2");
+})();
+
+((name)=>{
+    console.log(`welcme ${name}`);
+})("buddy")
+
